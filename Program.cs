f671fc4d6045294cf1836cd6e5988fc6ae6e4132
João Vitor Soares <jvfs12th@gmail.com>
2024-01-04 12:20:03 -0300

    :rocket: feat: Cria métodos de criar e atualizar posições da cobra

--- Program.cs
+++ Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SnakeGameCsharp.enums;
+using SnakeGameCsharp.game;
+using System;
+
+
+const int larguraTela = 69;
+const int alturaTela = 29;
+const string caractereCobra = "+=+";
+
+string[,] tela = new string[larguraTela, alturaTela];
+bool jogoRodando = true;
+
+List<Coordenada> coordenadasCobra = new();
+Direcao direcao = Direcao.Direita;
+int placar = 0;
+Random random = new();
+
+IniciarJogo();
+
+
+
+void IniciarJogo()
+{
+    CriarCobra();
+    CriarComida();
+    LerTeclas();
+
+    while (jogoRodando)
+    {
+        Thread.Sleep(50);
+        TransladarCobra();
+        Renderizar();
+    }
+    FimDeJogo();
+}
+
+void FimDeJogo()
+{
+    throw new NotImplementedException();
+}
+
+void Renderizar()
+{
+    throw new NotImplementedException();
+}
+
+void TransladarCobra()
+{
+    throw new NotImplementedException();
+}
+
+void LerTeclas()
+{
+    throw new NotImplementedException();
+}
+
+void CriarComida()
+{
+    throw new NotImplementedException();
+}
+
+void CriarCobra()
+{
+    coordenadasCobra.Add(new Coordenada(7,14));
+    coordenadasCobra.Add(new Coordenada(7, 14));
+    coordenadasCobra.Add(new Coordenada(7, 14));
+
+    AtualizarPosicaoCobra();
+
+}
+
+void AtualizarPosicaoCobra()
+{
+    for(int l = 0; l < larguraTela; l++)
+    {
+        for(int a = 0; a < alturaTela; a++)
+        {
+            var posicaoDeveConterCobra = coordenadasCobra.Any(coordenada => coordenada.X == l && coordenada.Y == a);
+
+            if (posicaoDeveConterCobra)
+            {
+                tela[l, a] = caractereCobra;
+                continue;
+            }
+
+            if (tela[l,a] == caractereCobra)
+            {
+                tela[l, a] = " ";
+            }
+        }
+    }
+}
---
+++ game/Coordenada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGameCsharp.game
+{
+    public class Coordenada
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordenada(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
